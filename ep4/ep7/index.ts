122624092b32697caa7ccdd0aadfdc1a6991d59f
magorn2003 <kitsanakran495@gmail.com>
2024-08-30 14:49:03 +0700

    ep7

---
+++ ep4/ep7/index.ts
@@ -0,0 +1,15 @@
+const animal: string ='แมว*หมี*มด*หมา*'
+
+const animalToArray: string[] = animal.split('*')
+
+console.log(animalToArray)
+
+console.log("ตั้งต้น: ", animalArray)
+
+animalArray.pop() //เอาตัวสุดท้ายออก
+
+console.log("เอาตัวสุดท้ายออก: ", animalArray)
+
+animalArray.push('กบ') //ใส่ต่อท้าย
+
+console.log("ใส่ต่อท้าย: ",)
